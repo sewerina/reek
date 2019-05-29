f2272192930aded4a62351573d3580b1f6e06ca4
elena <sewerina.ea@gmail.com>
2019-05-29 15:45:14 +0300

    get the map screen && show it in MainActivity

--- app/src/main/java/com/github/sewerina/reek/MainActivity.java
+++ app/src/main/java/com/github/sewerina/reek/MainActivity.java
@@ -2,27 +2,32 @@ package com.github.sewerina.reek;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAP_REQUEST_CODE = 13;
     private Spinner mSpinner;
     private Button mSelectLocationBtn;
+    private ImageView mMapScreenIv;
     private Button mSendComplaintBtn;
-
     private ReekSpinnerAdapter mSpinnerAdapter;
-
     private MainViewModel mViewModel;
 
     @Override
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         mSpinner = findViewById(R.id.spinner);
         mSelectLocationBtn = findViewById(R.id.btn_selectLocation);
+        mMapScreenIv = findViewById(R.id.iv_mapScreen);
         mSendComplaintBtn = findViewById(R.id.btn_sendComplaint);
 
         mSpinnerAdapter = new ReekSpinnerAdapter(this, R.layout.reek_row, reeks);
@@ -57,11 +63,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mSelectLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(v.getContext(), MapsActivity.class), MAP_REQUEST_CODE);
+            }
+        });
+
         mViewModel = ViewModelProviders.of(this, ReekApp.getViewModelFactory()).get(MainViewModel.class);
 
 
 //        startActivity(new Intent(this, MapsActivity.class));
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MAP_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            String filePath = data.getStringExtra(MapsActivity.EXTRA_FILE_PATH);
+            Log.d("MainActivity", "onActivityResult: filePath = " + filePath);
+
+            showMapScreen(filePath);
+        }
+    }
+
+    private void showMapScreen(String filePath) {
+        Bitmap screen = BitmapFactory.decodeFile(filePath);
+        mMapScreenIv.setImageBitmap(screen);
+        mMapScreenIv.setVisibility(View.VISIBLE);
     }
 
     @Override
--- app/src/main/java/com/github/sewerina/reek/MapsActivity.java
+++ app/src/main/java/com/github/sewerina/reek/MapsActivity.java
@@ -3,12 +3,15 @@ package com.github.sewerina.reek;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -22,22 +25,28 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String[] LOCATION_PERMISSIONS = new String[]
-            {Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION};
+            {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     private static final int REQUEST_LOCATION_PERMISSIONS = 0;
     private GoogleMap mMap;
+    private FloatingActionButton mSaveFab;
+    public static final String EXTRA_FILE_PATH = "filePath";
     private MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        mSaveFab = findViewById(R.id.fab_save);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -49,14 +58,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mViewModel.getReekObjects().observe(this, new Observer<List<ReekObject>>() {
             @Override
             public void onChanged(List<ReekObject> reekObjects) {
-//                StringBuilder builder = new StringBuilder();
-//
-//                for (ReekObject reek : reekObjects) {
-//                    builder.append(reek.mName).append("\n");
-//                }
-//                mTextView.setText(builder);
-
                 showReeks(reekObjects);
+            }
+        });
+
+        mSaveFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureMapScreen();
             }
         });
     }
@@ -66,7 +75,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
 
-        for (ReekObject reek: reekObjects) {
+        for (ReekObject reek : reekObjects) {
             LatLng point = new LatLng(reek.mLatitude, reek.mLongitude);
             mMap.addMarker(new MarkerOptions().position(point).title(reek.mName));
 //            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
@@ -115,6 +124,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    private void captureMapScreen() {
+        GoogleMap.SnapshotReadyCallback callback = new GoogleMap.SnapshotReadyCallback() {
+            Bitmap bitmap;
+            @Override
+            public void onSnapshotReady(Bitmap snapshot) {
+                // TODO Auto-generated method stub
+                bitmap = snapshot;
+                try {
+                    File externalCacheDir = MapsActivity.this.getExternalCacheDir();
+                    File tempFile = File.createTempFile("map" + System.currentTimeMillis(), ".png", externalCacheDir);
+                    FileOutputStream out = new FileOutputStream(tempFile);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+
+                    Log.d("MapsActivity", "onSnapshotReady: " + bitmap.toString());
+
+                    sendData(tempFile.getPath());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mMap.snapshot(callback);
+    }
+
+    private void sendData(String filePath) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_FILE_PATH, filePath);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+
     private void setCurrentLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -130,7 +171,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
             mMap.addMarker(new MarkerOptions().position(last).title("Marker"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(last));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(last));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(last, 18));
+
         }
     }
 
