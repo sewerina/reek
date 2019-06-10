b2f0be413ab1b1194ce988ae437065a2047b6d02
elena <sewerina.ea@gmail.com>
2019-06-10 11:21:05 +0300

    add new reek kind in spinner

--- app/src/main/java/com/github/sewerina/reek/ReekKindList.java
+++ app/src/main/java/com/github/sewerina/reek/ReekKindList.java
@@ -8,6 +8,7 @@ public class ReekKindList extends ArrayList<ReekKind> {
         add(new ReekKind("Запах канализации", "канализации", R.drawable.canalization));
         add(new ReekKind("Сероводород (запах тухлых яиц)", "cероводорода", R.drawable.eggs));
         add(new ReekKind("Зловоние от свалки мусора/Свалочный газ", "свалочного газа", R.drawable.garbage));
+        add(new ReekKind("Химический запах", "непонятной химии", R.drawable.chem));
         add(new ReekKind("Выхлопы от автомобилей", "выхлопных газов", R.drawable.exhaust));
     }
 }
