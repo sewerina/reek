25bb110cca760d66edd14e7af3ff584e3205a893
elena <sewerina.ea@gmail.com>
2019-06-11 23:40:34 +0300

    add test for Menu in UIMainTest

--- app/src/androidTest/java/com/github/sewerina/reek/UIMainTest.java
+++ app/src/androidTest/java/com/github/sewerina/reek/UIMainTest.java
@@ -1,22 +1,28 @@
 package com.github.sewerina.reek;
 
 import android.content.pm.ActivityInfo;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -24,6 +30,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -104,6 +111,25 @@ public class UIMainTest {
         onView(withText(startsWith("Губер")))
                 .check(matches(isDisplayed()))
                 .check(matches(isChecked()));
+    }
+
+    @Test
+    public void testMenu() {
+        Intents.init();
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText(R.string.menuItem_appInfo))
+                .check(matches(instanceOf(AppCompatTextView.class)))
+                .check(matches(isDisplayed()))
+                .check(matches(isEnabled()))
+                .perform(click());
+
+        intended(hasComponent(AppInfoActivity.class.getName()));
+
+        onView(withId(R.id.tv_appInfo))
+                .check(matches(instanceOf(TextView.class)))
+                .check(matches(isDisplayed()));
     }
 
 }
