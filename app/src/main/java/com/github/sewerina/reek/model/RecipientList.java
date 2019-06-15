c621c98d516d238c09438d309e35ddfe7907479a
elena <sewerina.ea@gmail.com>
2019-06-15 23:03:04 +0300

    prepare release

--- app/src/main/java/com/github/sewerina/reek/model/RecipientList.java
+++ app/src/main/java/com/github/sewerina/reek/model/RecipientList.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 
 public class RecipientList extends ArrayList<Recipient> {
     public RecipientList() {
-        // String[] emailArray = {"rpn77@rpn.gov.ru", "gu_moscov@mchs.gov.ru", "andreyvorobiev@mosreg.ru"};
-        add(new Recipient("Департамент Росприроднадзора по Центральному федеральному округу","A@.AA"));
-        add(new Recipient("Главное управление МЧС России по г. Москве","B@.BB"));
-        add(new Recipient("Главное управление МЧС России по Московской области (общий отдел)","С@.СС"));
-        add(new Recipient("Губернатор Московской области","D@.DD"));
-        add(new Recipient("Общероссийская Общественная организация «Зелёный патруль»","E@.EE"));
+        add(new Recipient("Департамент Росприроднадзора по Центральному федеральному округу","rpn77@rpn.gov.ru"));
+        add(new Recipient("Главное управление МЧС России по г. Москве","gu_moscov@mchs.gov.ru"));
+        add(new Recipient("Главное управление МЧС России по Московской области (общий отдел)","obshiy_mchs_mo@mail.ru"));
+        add(new Recipient("Губернатор Московской области","andreyvorobiev@mosreg.ru"));
+        add(new Recipient("Общероссийская Общественная организация «Зелёный патруль»","russia@greenpatrol.ru"));
     }
 }
