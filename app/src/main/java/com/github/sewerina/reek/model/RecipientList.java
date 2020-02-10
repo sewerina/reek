ab3fef5395528449cb78ea376b28171c7de4041f
elena <sewerina.ea@gmail.com>
2020-02-10 14:21:24 +0300

    delete some emails

--- app/src/main/java/com/github/sewerina/reek/model/RecipientList.java
+++ app/src/main/java/com/github/sewerina/reek/model/RecipientList.java
@@ -8,9 +8,6 @@ public class RecipientList extends ArrayList<Recipient> {
         add(new Recipient("Межрегиональное Управление Росприроднадзора по Московской и Смоленской областям", "rpn67@rpn.gov.ru"));
         add(new Recipient("Департамент природопользования и охраны окружающей среды г. Москвы", "depmospriroda@mos.ru"));
         add(new Recipient("Министерство экологии и природопользования Московской области", "minecology@mosreg.ru"));
-        add(new Recipient("Главное управление МЧС России по г. Москве", "gu_moscov@mchs.gov.ru"));
-        add(new Recipient("Главное управление МЧС России по Московской области (общий отдел)", "obshiy_mchs_mo@mail.ru"));
         add(new Recipient("Губернатор Московской области", "andreyvorobiev@mosreg.ru"));
-        add(new Recipient("Общероссийская Общественная организация «Зелёный патруль»", "russia@greenpatrol.ru"));
     }
 }
